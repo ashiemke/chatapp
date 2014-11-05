a1d22161b330c2a8a4972ffd1dc99f9c92f0b0ce
Adam Shiemke <ashiemke@amadeusconsulting.com>
2014-11-05 11:44:43 -0700

    Initial Commit

---
+++ chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chat
+//
+//  Created by Adam Shiemke on 11/5/14.
+//  Copyright (c) 2014 Adam Shiemke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ chat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  chat
+//
+//  Created by Adam Shiemke on 11/5/14.
+//  Copyright (c) 2014 Adam Shiemke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
